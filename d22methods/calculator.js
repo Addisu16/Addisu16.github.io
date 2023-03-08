fc255860a309394df545f702c33933be43310bf6
Addisu16 <71025422+Addisu16@users.noreply.github.com>
2023-03-08 11:03:14 -0600

    index

--- d22methods/calculator.js
+++ d22methods/calculator.js
@@ -1,14 +1,17 @@
-"use strict";
+//"use strict";
 
-const calculator = {};  // implement this
+export const Calculator = {
+    a:0,
+    b:0,
+    setValues: function(aValue,bValue){
+      this.a=aValue;
+      this.b=bValue;
+    },  
 
-
-
-
-
-
-
-
-/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-/* must be at end of file if are exporting an object so the export is after the definition */
-module.exports = {calculator }; //add all of your object names here that you need for the node mocha tests
+sum:function(){
+    return this.a+this.b;// implement this
+},
+mul:function(){
+    return this.a*this.b;
+}
+};
